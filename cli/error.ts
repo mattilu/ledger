e76d77655eb38bcc716507903a2122fdcf477ec6
Luca Emanuele Mattiello <lucae.mattiello@gmail.com>
2025-05-01 19:17:14 +0200

    cli: print stack trace for load errors

--- cli/error.ts
+++ cli/error.ts
@@ -3,8 +3,14 @@ import { LoadError } from '../lib/loading/error.js';
 
 export class CommandError extends Error {
   static fromLoadError(error: LoadError): CommandError {
+    const stackTrace = error.stackTrace.map(
+      srcCtx => `  loaded from ${srcCtx.filePath}:${srcCtx.row}`,
+    );
     return new CommandError(
-      `${error.message} at ${error.srcCtx.filePath}:${error.srcCtx.row}`,
+      [
+        `${error.message} at ${error.srcCtx.filePath}:${error.srcCtx.row}`,
+        ...stackTrace,
+      ].join('\n'),
       { cause: error },
     );
   }
